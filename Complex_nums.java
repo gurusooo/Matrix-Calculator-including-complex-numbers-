4188e779c7502de076b723dfa9bf6642ba22c86d
gurusooo <130143477+gurusooo@users.noreply.github.com>
2024-02-20 22:59:50 +0300

    Update Complex_nums.java

--- Complex_nums.java
+++ Complex_nums.java
@@ -34,7 +34,7 @@ class Complex_nums
     }
     public Complex_nums multiply (Complex_nums second)
     {
-        double realpart = this.real * second.real - this.im * second.im;
+        double realpart = this.real * second.real- this.im * second.im;
         double impart = this.real * second.im + this.im * second.real;
         return new Complex_nums(realpart, impart);
     }
